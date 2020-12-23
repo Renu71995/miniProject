ab2c81c694435666d3efe0dea7c4e497e9dfa1dc
unknown <preethy.chettickal@ibsplc.com>
2020-12-23 17:23:36 +0530

    booking changes updated

--- src/main/java/com/cargo/entities/BasicBookingDetails.java
+++ src/main/java/com/cargo/entities/BasicBookingDetails.java
@@ -1,39 +1,81 @@
 package com.cargo.entities;
 
 import java.io.Serializable;
+import java.util.Random;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
 
 @Entity
 public class BasicBookingDetails implements Serializable {
 
-	/** Default Serializable uid **/
+	/**
+	 * 
+	 */
 	private static final long serialVersionUID = 1L;
-	@Id
-	private Integer awbNumber;
-	private Integer ubrNumber;
-	private String awbStatus;
-	private String origin;
-	private String destination;
-	private String flightNumber;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "flightNumber", referencedColumnName = "flightNumber", updatable = false, insertable = false)
-	private FlightDetails flightDetails;
-	@ManyToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "awbNumber", referencedColumnName = "awbNumber", updatable = false, insertable = false)
-	private ULDdetails uldDetails;
-	@OneToOne(fetch = FetchType.LAZY)
-	@JoinColumn(name = "awbNumber", referencedColumnName = "awbNumber", updatable = false, insertable = false)
-	private BulkBooking bulkbooking;
 
-	public BasicBookingDetails(Integer awbNumber, Integer ubrNumber, String awbStatus, String origin,
-			String destination, String flightNumber, FlightDetails flightDetails, ULDdetails uldDetails,
-			BulkBooking bulkbooking) {
+	@Id
+	//@GeneratedValue(strategy=GenerationType.AUTO)
+//	@GeneratedValue(strategy=GenerationType.SEQUENCE,generator="awb_number")
+//	@GenericGenerator(name="awb_number",strategy="com.cargo.entities.IdGenerator")
+	private int awbNumber;
+	@Column(nullable = false)
+	private String ubrNumber;
+	private String awbStatus;
+	@Column(nullable = false)
+	private String origin;
+	@Column(nullable = false)
+	private String destination;
+	@Column(nullable = false)
+	private String flightNumber;
+	@Column(nullable = false)
+	private String agentCode;
+	@Column(nullable = false)
+	private String agentName;
+	@Column(nullable = false)
+	private String customerCode;
+	@Column(nullable = false)
+	private String customerName;
+	@Column(nullable = false)
+	private String shippingDate;
+	@Column(nullable = false)
+	private String scc;
+	@Column(nullable = false)
+	private String serviceClass;
+	@Column(nullable = false)
+	private String flightDetailsOrigin;
+	@Column(nullable = false)
+	private String flightDetailsDestination;
+	@Column(nullable = false)
+	private String flightDate;
+
+	public String getFlightDetailsOrigin() {
+		return flightDetailsOrigin;
+	}
+	public void setFlightDetailsOrigin(String flightDetailsOrigin) {
+		this.flightDetailsOrigin = flightDetailsOrigin;
+	}
+	public String getFlightDetailsDestination() {
+		return flightDetailsDestination;
+	}
+	public void setFlightDetailsDestination(String flightDetailsDestination) {
+		this.flightDetailsDestination = flightDetailsDestination;
+	}
+	public String getFlightDate() {
+		return flightDate;
+	}
+	public void setFlightDate(String flightDate) {
+		this.flightDate = flightDate;
+	}
+	public BasicBookingDetails() {
+	// TODO Auto-generated constructor stub
+  	}
+	public BasicBookingDetails(int awbNumber, String ubrNumber, String awbStatus, String origin, String destination,
+			String flightNumber, String agentCode, String agentName, String customerCode, String customerName,
+			String shippingDate, String scc, String serviceClass
+
+			) {
 		super();
 		this.awbNumber = awbNumber;
 		this.ubrNumber = ubrNumber;
@@ -41,84 +83,176 @@ public class BasicBookingDetails implements Serializable {
 		this.origin = origin;
 		this.destination = destination;
 		this.flightNumber = flightNumber;
-		this.flightDetails = flightDetails;
-		this.uldDetails = uldDetails;
-		this.bulkbooking = bulkbooking;
+		this.agentCode = agentCode;
+		this.agentName = agentName;
+		this.customerCode = customerCode;
+		this.customerName = customerName;
+		this.shippingDate = shippingDate;
+		this.scc = scc;
+		this.serviceClass = serviceClass;
+
 	}
 
-	public Integer getAwbNumber() {
+
+	public int getAwbNumber() {
 		return awbNumber;
 	}
 
-	public void setAwbNumber(Integer awbNumber) {
+
+	public void setAwbNumber(int awbNumber) {
 		this.awbNumber = awbNumber;
 	}
+	
+	public void setAwbNumber(){
+		 Random r = new Random( System.currentTimeMillis());
+		 this.awbNumber=75700000 + r.nextInt(20000);
+	}
 
-	public Integer getUbrNumber() {
+
+	public String getUbrNumber() {
+		 
+		
 		return ubrNumber;
 	}
 
-	public void setUbrNumber(Integer ubrNumber) {
-		this.ubrNumber = ubrNumber;
+
+	public void setUbrNumber(String ubrNumber) {
+		this.ubrNumber=ubrNumber;
+	}
+	
+	public void setUbrNumber(String origin,String destination,int randomnumber) {
+		this.ubrNumber=origin+destination+randomnumber;
 	}
 
 	public String getAwbStatus() {
 		return awbStatus;
 	}
 
+
 	public void setAwbStatus(String awbStatus) {
 		this.awbStatus = awbStatus;
 	}
+
 
 	public String getOrigin() {
 		return origin;
 	}
 
+
 	public void setOrigin(String origin) {
 		this.origin = origin;
 	}
+
 
 	public String getDestination() {
 		return destination;
 	}
 
+
 	public void setDestination(String destination) {
 		this.destination = destination;
 	}
+
 
 	public String getFlightNumber() {
 		return flightNumber;
 	}
 
+
 	public void setFlightNumber(String flightNumber) {
 		this.flightNumber = flightNumber;
 	}
 
-	public FlightDetails getFlightDetails() {
-		return flightDetails;
+
+	public String getAgentCode() {
+		return agentCode;
 	}
 
-	public void setFlightDetails(FlightDetails flightDetails) {
-		this.flightDetails = flightDetails;
+
+	public void setAgentCode(String agentCode) {
+		this.agentCode = agentCode;
 	}
 
-	public ULDdetails getUldDetails() {
-		return uldDetails;
+
+	public String getAgentName() {
+		return agentName;
 	}
 
-	public void setUldDetails(ULDdetails uldDetails) {
-		this.uldDetails = uldDetails;
+
+	public void setAgentName(String agentName) {
+		this.agentName = agentName;
 	}
 
-	public BulkBooking getBulkbooking() {
-		return bulkbooking;
+
+	public String getCustomerCode() {
+		return customerCode;
 	}
 
-	public void setBulkbooking(BulkBooking bulkbooking) {
-		this.bulkbooking = bulkbooking;
+
+	public void setCustomerCode(String customerCode) {
+		this.customerCode = customerCode;
 	}
+
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+
+	public String getShippingDate() {
+		return shippingDate;
+	}
+
+
+	public void setShippingDate(String shippingDate) {
+		this.shippingDate = shippingDate;
+	}
+
+
+	public String getScc() {
+		return scc;
+	}
+
+
+	public void setScc(String scc) {
+		this.scc = scc;
+	}
+
+
+	public String getServiceClass() {
+		return serviceClass;
+	}
+
+
+	public void setServiceClass(String serviceClass) {
+		this.serviceClass = serviceClass;
+	}
+
+
+
 
 	public static long getSerialversionuid() {
 		return serialVersionUID;
 	}
+	@Override
+	public String toString() {
+		return "BasicBookingDetails [awbNumber=" + awbNumber + ", ubrNumber=" + ubrNumber + ", awbStatus=" + awbStatus
+				+ ", origin=" + origin + ", destination=" + destination + ", flightNumber=" + flightNumber
+				+ ", agentCode=" + agentCode + ", agentName=" + agentName + ", customerCode=" + customerCode
+				+ ", customerName=" + customerName + ", shippingDate=" + shippingDate + ", scc=" + scc
+				+ ", serviceClass=" + serviceClass + ", bulkBooking="
+//				+ bulkBooking + ", uldDetails=" + uldDetails
+				+ "]";
+	}
+	
+	
+	
+
+
+	
 }
